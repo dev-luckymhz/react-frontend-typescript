74b23f071bdc5c1b6d02311a71f122f1ad942203
Zo-ambinintsoa <nambinintsoa577@gmail.com>
2023-02-15 22:33:00 -0800

    updating userService for handling user request

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -5,18 +5,52 @@ interface UserData {
     username: string,
     password: string,
     email: string,
-    confirmPassword: string
+    confirmPassword?: string
 }
 
-export const RegisterUser = async (body: UserData , url: string) => {
-        axios({
-            method: 'post',
-            url: url,
-            data: body
-        })
-            .then(function (response) {
-                return response
-            }).catch(err => {
-            return err
-        });
+const UserBaseUrl = "http:/localhost:8000/api/user"
+
+const UserAxiosRequest = async (requestType = "get", body?: UserData | null, url = UserBaseUrl) => {
+    axios({
+        method: requestType,
+        url: url,
+        data: body
+    })
+        .then(function (response) {
+            return response
+        }).catch(err => {
+        return err
+    });
+}
+
+export const RegisterUser = async (data: UserData , url: string) => {
+    return UserAxiosRequest("post", data)
+}
+
+export const UpdateUser = async (body: UserData , url: string) => {
+    const userId = body.id
+    axios({
+        method: 'put',
+        url: url,
+        data: body
+    })
+        .then(function (response) {
+            return response
+        }).catch(err => {
+        return err
+    });
+}
+
+export const DeleteUser = async (body: UserData , url: string) => {
+    const userId = body.id
+    axios({
+        method: 'delete',
+        url: url,
+        data: body
+    })
+        .then(function (response) {
+            return response
+        }).catch(err => {
+        return err
+    });
 }
